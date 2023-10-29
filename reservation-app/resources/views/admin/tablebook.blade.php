b8bdd17a6988f8414d5d9969ed29eea4f8e70dca
Yagya-Adh <yagyaadhikari02@gmail.com>
2023-10-29 13:17:28 +0545

    Create: created Booking Tables not arranged .

--- reservation-app/resources/views/admin/tablebook.blade.php
+++ reservation-app/resources/views/admin/tablebook.blade.php
@@ -2,19 +2,100 @@
    
 </x-app-layout>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
+
    @include('admin.admincss')
+
+    <script src="https://code.jquery.com/jquery-3.7.1.js"
+        integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+
+   
+  
   </head>
 
+  <style>
 
-  <style> 
+
+/* block a css  */
+
+.block_a{
+
+  display: flex;
+  justify-content: center;
+
+  max-width: 150px !important;
+  height: 600px;
+
+  border: 2px solid #ccc;
+}
+
+
+.btnDiv{
+  padding: 4px;
+}
+ 
+.btnDiv span{
+  display: flex;
+  padding: 3px;
+  border: 1px solid gray;
+  width: 40px;
+  justify-content: center;
+  margin: 3px;
+}
+
+.btnDiv span:hover{
+  display: flex;
+  padding: 3px;
+  border: 1px solid gray;
+  width: 40px;
+  background: rgb(80, 161, 199);
+
+}
+
+
+/* block b css */
+.block_b{
+  padding: 40px;
+  border: 2px solid #ccc;
+  display: flex;
+  justify-content: center;
+}
+
+.block_b div span{
+  margin: 20px;
+  padding: 30px;
+  display: flex;
+  border: 2px solid greenyellow;
+
+  background-image: url("admin/assets/images/table_book.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  
+  justify-content: center;
+
+  max-width: 180px !important;
+  height: 180px !important;
+}
+
+
+.block_b div span:hover{
+  background-color: teal;
+
+  background-image: url("admin/assets/images/table_empty.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  
+  justify-content: center;
+
+ }
 
   </style>
-  <body>
+
+  
+  <body >
     <div class="container-scroller">
      
       <!-- partial:partials/_sidebar.html -->
@@ -28,15 +109,52 @@
                     <div class="main-panel">
                     <div class="content-wrapper">
                         
-                        <div class="row">
-                         
-                            <h1>Table Book</h1>
+                      <div class="row" style="background: rgb(255, 255, 255); color:black; padding:40px;">
+
+                            <h1>Table Book</h1>   
+                             
+                              <div class="row">
+                                
+                                <div class="col block_a"> 
+                                    
+                                     <div class="col">
+                                       <h1>Select Blook</h1>
+                                      <div class="btnDiv">
+                                          <span>1</span>
+                                          <span>2</span>
+                                          <span>3</span>
+                                          <span>4</span>
+                                          <span>5</span>
+                                          <span>6</span>
+                                          <span>7</span>
+                                          <span>8</span>
+                                          <span>9</span>
+                                      </div>
+                                     </div>
+                                  
  
 
- 
-
-
-                        </div>
+                                </div>
+                              
+                                {{-- block b --}}
+                              
+                                <div class="col block_b">                                 
+                                  <div class="row">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                  </div>
+                                </div>
+                              </div>
+                           
+                              
+                      </div>
                          
                     </div>
                     <!-- content-wrapper ends -->
@@ -55,17 +173,30 @@
     </div>
         @include('admin.adminscript')
 
- <script>
-      function changeColor(color) {
-        var largeCircle = document.getElementById('large-circle');
-        if (color === 'green') {
-          largeCircle.style.backgroundColor = '#2ecc71';
-        } else if (color === 'gray') {
-          largeCircle.style.backgroundColor = '#ccc';
-        }else{
-          largeCircle.style.backgroundColor =  '#032e4b';
-        }
-      }
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+
+
+    <script>
+
+        $(document).ready(function () {
+
+
+            $("#boxs_1").click(function () {
+                $(".table_a").addClass("defaultClass");
+            });
+
+            $("#paybtn").click(function () {
+                confirm('payyour bill');
+                $(".table_a").removeClass("defaultClass");
+                $(".table_a").css('background-color', 'green');
+            })
+
+
+          });
+
     </script>
 
   
